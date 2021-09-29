82bc8d9e02bd6fd880c51646a570796482a8cf99
Shawn Rice <Shawn.CS@protonmail.com>
2021-09-29 03:56:17 -0400

    Added extra database calls to return new objects

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,7 +1,6 @@
-// implement your posts router here
-const express = require('express');
-const Post = require('./posts-model');
-const router = express.Router();
+const express = require('express')
+const Post = require('./posts-model')
+const router = express.Router()
 
 router.get('/', (req, res) => {
   Post.find(req.query)
@@ -10,7 +9,11 @@ router.get('/', (req, res) => {
     })
     .catch(err => {
       console.log(err)
-      res.status(500).json({ message: "The posts information could not be retrieved" })
+      res.status(500).json({ 
+        message: "The posts information could not be retrieved",
+        err: err.message,
+        stack: err.stack
+      })
     })
 })
 
@@ -25,7 +28,11 @@ router.get('/:id', (req, res) => {
     })
     .catch(err => {
       console.log(err)
-      res.status(500).json({ message: "The post information could not be retrieved" })
+      res.status(500).json({ 
+        message: "The post information could not be retrieved",
+        err: err.message,
+        stack: err.stack 
+      })
     })
 })
 
@@ -34,48 +41,71 @@ router.post('/', (req, res) => {
     res.status(400).json({ message: "Please provide title and contents for the post" })
   } else {
     Post.insert(req.body)
+      .then(({ id }) => {
+        return Post.findById(id)
+      })
       .then(post => {
         res.status(201).json(post)
       })
       .catch(err => {
         console.log(err)
-        res.status(500).json({ message: "There was an error while saving the post to the database" })
+        res.status(500).json({ 
+          message: "There was an error while saving the post to the database",
+          err: err.message,
+          stack: err.stack 
+        })
       })
   }
 })
 
 router.put('/:id', (req, res) => {
-  const update = req.body;
+  const update = req.body
+  const id = req.params.id
+
   if (!update.title || !update.contents) {
     res.status(400).json({ message: "Please provide title and contents for the post" })
   } else {
-    Post.update(req.params.id, update)
-    .then(post => {
-      if (!post) {
-        res.status(404).json({ message: "The post with the specified ID does not exist" })
-      } else {
-        res.status(200).json(post)
-      }
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ message: "The post information could not be modified" })
-    })
+    Post.update(id, update)
+      .then(id => {
+        return Post.findById(id)
+      })
+      .then(post => {
+        if (!post) {
+          res.status(404).json({ message: "The post with the specified ID does not exist" })
+        } else {
+          res.status(200).json(post)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ 
+          message: "The post information could not be modified",
+          err: err.message,
+          stack: err.stack 
+        })
+      })
   }
 })
 
-router.delete('/:id', (req, res) => {
-  Post.remove(req.params.id)
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  const post = await Post.findById(id)
+  
+  Post.remove(id)
     .then(records => {
       if (records > 0) {
-        res.status(200).json(records)
+        res.status(200).json(post)
       } else {
         res.status(404).json({ message: "The post with the specified ID does not exist" })
       }
     })
     .catch(err => {
       console.log(err)
-      res.status(500).json({ message: "The post could not be removed" })
+      res.status(500).json({ 
+        message: "The post could not be removed",
+        err: err.message,
+        stack: err.stack 
+      })
     })
 })
 
@@ -91,13 +121,21 @@ router.get('/:id/comments', (req, res) => {
           })
           .catch(err => {
             console.log(err)
-            res.status(500).json({ message: "The comments information could not be retrieved" })
+            res.status(500).json({ 
+              message: "The comments information could not be retrieved",
+              err: err.message,
+              stack: err.stack 
+            })
           })
       }
     })
     .catch(err => {
       console.log(err)
-      res.status(500).json({ message: "The post information could not be retrieved" })
+      res.status(500).json({ 
+        message: "The post information could not be retrieved",
+        err: err.message,
+        stack: err.stack 
+      })
     })
 })
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // require your server and launch it here
-const server = require('./api/server');
+const server = require('./api/server')
 
 server.listen(5000, () => {
   console.log('Server listening on http://localhost:5000')
